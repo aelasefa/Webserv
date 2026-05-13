aefce92cbd48248b9cccf0f2b961cc3b5448dff6
aylaaouf <ayoub.laaoufi@hotmail.com>
2026-05-13 18:58:08 +0100

    update arch and integrate cgi with response

--- includes/Cgi.hpp
+++ includes/Cgi.hpp
@@ -2,25 +2,31 @@
 #define CGI_HPP
 
 #include <string>
+#include <vector>
 #include <unistd.h>
 #include <sys/wait.h>
 #include <sys/types.h>
+#include <stdexcept>
 #include "Response.hpp"
 
-class CGI
+class Cgi
 {
 	private:
 		std::string _scriptPath;
 		std::string _interpreter;
-
 	public:
-		CGI();
-		~CGI();
+		Cgi();
+		~Cgi();
 
 		void setScriptPath(const std::string& path);
 		void setInterpreter(const std::string& interpreter);
-
+		std::vector<std::string> setEnv(const Request& request);
 		std::string execute(const Request& request);
+};
+
+class CgiHandler {
+	public:
+		static Response buildResponse(const std::string& cgiOutput);
 };
 
 #endif
